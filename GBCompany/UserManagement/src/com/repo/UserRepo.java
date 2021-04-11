8ad1bd2700fe6dec49b8eccc0fbaa03805e0b67b
Heesha14 <heesha14@gmail.com>
2021-04-12 00:27:01 +0530

    User Management / HeeshaJ

---
+++ GBCompany/UserManagement/src/com/repo/UserRepo.java
@@ -0,0 +1,42 @@
+package com.repo;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import com.model.Users;
+
+/**
+ * @author HeeshaJ
+ *
+ */
+public interface UserRepo {
+
+	/**
+	 *
+	 * @param username
+	 * @param password
+	 * @param email
+	 * @param phone
+	 * @param gender
+	 * @param designation
+	 * @return
+	 */
+	public String createUser(String username, String password, String email, String phone, String gender,String designation);
+
+
+	/**
+	 * Gets all user details from User table
+	 *
+	 * @return the user details.
+	 * @throws SQLException
+	 */
+	public List<Users> getUsersList() throws SQLException;
+
+	/**
+	 * Delete user respectively
+	 *
+	 * @return the deleted message.
+	 * @throws SQLException
+	 */
+	public String deleteUser(String userId) throws SQLException;
+}
---
+++ GBCompany/UserManagement/src/com/service/UserServiceImpl.java
@@ -0,0 +1,31 @@
+package com.service;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import com.model.Users;
+import com.repo.UserRepo;
+import com.repo.UserRepoImpl;
+
+public class UserServiceImpl implements UserService {
+
+	UserRepo userRepo = new UserRepoImpl();
+
+	@Override
+	public String createUser(String username, String password, String email, String phone, String gender,
+							 String designation) {
+		return userRepo.createUser(username, password, email, phone, gender, designation);
+	}
+
+	@Override
+	public List<Users> getAllUsers() throws SQLException {
+		return userRepo.getUsersList();
+	}
+
+	@Override
+	public String deleteUser(String userId) throws SQLException {
+		System.out.println("deleteUser service");
+		return userRepo.deleteUser(userId);
+	}
+
+}
