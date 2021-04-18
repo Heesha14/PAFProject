a05b5eb974bee260951b25a443cb103c66307412
HeshikiHalpe <heshikihalpe@gmail.com>
2021-04-18 23:50:58 +0530

    fund payment insert operation. fifth commit

--- GBCompany/PaymentManagement/src/main/java/model/Payment.java
+++ GBCompany/PaymentManagement/src/main/java/model/Payment.java
@@ -65,6 +65,42 @@ public class Payment {
 	}
 	
 	//insert fund payment
-	
+	public String insertFundPayment(int fundID, String amount, Date paidDate)
+	{
+	 
+		String output = "";
+	 
+		try
+	 
+		{	 
+			Connection con = connect();
+	 
+			if (con == null)	 
+			{return "Error while connecting to the database for inserting."; }
+	 
+			// create a prepared statement	 
+			String query = " insert into fund_payments(`fundPayid`,`fundId`,`amount`,`paid_date`)"	 
+					+ " values (?, ?, ?, ?)";
+	 
+			PreparedStatement preparedStmt = con.prepareStatement(query);
+	 
+			// binding values	 
+			preparedStmt.setInt(1, 0);	 
+			preparedStmt.setInt(2, fundID);
+			preparedStmt.setDouble(3, Double.parseDouble(amount));
+			preparedStmt.setDate(4, paidDate);
+			
+			// execute the statement	 
+			preparedStmt.execute();	 
+			con.close();	 
+			output = "Inserted successfully";	 
+		} 
+		catch (Exception e) 
+		{ 
+			output = "Error while inserting the payment."; 
+			System.err.println(e.getMessage()); 
+		}	 
+		return output;
+	}
 
 }
