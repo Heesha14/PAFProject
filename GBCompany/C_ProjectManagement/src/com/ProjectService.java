d2b61bbac71f7eef4916327cb927731259d0d44f
Chamodi Karunathunge <krashinkaz@1998>
2021-04-20 21:01:33 +0530

    Implementation edit.

--- GBCompany/C_ProjectManagement/src/com/ProjectService.java
+++ GBCompany/C_ProjectManagement/src/com/ProjectService.java
@@ -36,39 +36,6 @@ public class ProjectService {
 		return output;
 	}
 
-	@PUT
-	@Path("/")
-	@Consumes(MediaType.APPLICATION_JSON)
-	@Produces(MediaType.TEXT_PLAIN)
-	public String updateProject(String projectData) {
-//Convert the input string to a JSON object 
-		JsonObject projectObject = new JsonParser().parse(projectData).getAsJsonObject();
-//Read the values from the JSON object
-		String Project_ID = projectObject.get("Project_ID").getAsString();
-		String pmid = projectObject.get("pmid").getAsString();
-		String fbid = projectObject.get("fbid").getAsString();
-		String Project_Name = projectObject.get("Project_Name").getAsString();
-		String Start_Date = projectObject.get("Start_Date").getAsString();
-		String Deadline_Date = projectObject.get("Deadline_Date").getAsString();
-		String Project_Status = projectObject.get("Project_Status").getAsString();
-		String Project_Fund_Amt = projectObject.get("Project_Fund_Amt").getAsString();
-		String Project_Sell_Amt = projectObject.get("Project_Sell_Amt").getAsString();
-		String output = projectObj.updateProject(Project_ID, pmid, fbid, Project_Name, Start_Date, Deadline_Date, Project_Status, Project_Fund_Amt, Project_Fund_Amt);
-		return output;
-	}
 
-	@DELETE
-	@Path("/")
-	@Consumes(MediaType.APPLICATION_XML)
-	@Produces(MediaType.TEXT_PLAIN)
-	public String deleteProject(String projectData) {
-//Convert the input string to an XML document
-		Document doc = Jsoup.parse(projectData, "", Parser.xmlParser());
-
-//Read the value from the element <Project_ID>
-		String Project_ID = doc.select("Project_ID").text();
-		String output = projectObj.deleteProject(Project_ID);
-		return output;
-	}
 
 }
