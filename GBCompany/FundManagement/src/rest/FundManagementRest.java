86cb71a01e34f7eba32aa6e60fda94f28a3ffbf5
Udayantha03 <it19167138@my.sliit.lk>
2021-04-25 20:17:59 +0530

    status update

--- GBCompany/FundManagement/src/rest/FundManagementRest.java
+++ GBCompany/FundManagement/src/rest/FundManagementRest.java
@@ -20,9 +20,11 @@ import org.jsoup.nodes.Document;
 public class FundManagementRest {
 	
 		//Fund service object
+	
 		FundService fundObj = new FundService(); 
 
 		//retrieve fund service
+		
 		@GET
 		@Path("RetrieveFunds") 
 		@Produces(MediaType.TEXT_HTML) 
@@ -33,6 +35,7 @@ public class FundManagementRest {
 		
 		
 		//add fund service
+		
 		@POST
 		@Path("AddFunds") 
 		@Consumes(MediaType.APPLICATION_FORM_URLENCODED) 
@@ -52,15 +55,17 @@ public class FundManagementRest {
 		
 		
 		//update fund service
+		
 		@PUT
 		@Path("UpdateFunds") 
 		@Consumes(MediaType.APPLICATION_JSON) 
 		@Produces(MediaType.TEXT_PLAIN) 
 		public String updateFund(String fundData) 
 		{ 
-			//Convert the input string to a JSON object 
+			//Convert the input string to a JSON object 			
 			JsonObject fundUpdateObject = new JsonParser().parse(fundData).getAsJsonObject(); 
-			//Read the values from the JSON object
+			
+			//Read the values from the JSON object	
 			String fundId = fundUpdateObject.get("fundId").getAsString(); 
 			String fundCode = fundUpdateObject.get("fundCode").getAsString(); 
 			String fundType = fundUpdateObject.get("fundType").getAsString(); 
@@ -76,6 +81,7 @@ public class FundManagementRest {
 		
 		
 		//delete fund service
+		
 		@DELETE
 		@Path("DeleteFunds") 
 		@Consumes(MediaType.APPLICATION_XML) 
@@ -83,11 +89,13 @@ public class FundManagementRest {
 		public String deleteFund(String fundData) 
 		{ 
 
-		//Convert the input string to an XML document
+		//Convert the input string to an XML document	
 		 Document document = Jsoup.parse(fundData, "", Parser.xmlParser()); 
 		 
-		//Read the values
+		//Read the values 
 		 String fundId = document.select("fundId").text(); 
+		 
+		 //output 
 		 String output = fundObj.deleteFund(fundId); 
 		
 		return output; 
@@ -95,28 +103,26 @@ public class FundManagementRest {
 		
 		
 	
-
-		//update fund status
-		@PUT
-		@Path("statusUpdate") 
-		@Consumes(MediaType.APPLICATION_JSON) 
-		@Produces(MediaType.TEXT_PLAIN) 
-		public String statusUpdate(String statusData) 
-		{ 
-			//Convert the input string to a JSON object 
-			JsonObject fundUpdateObject = new JsonParser().parse(statusData).getAsJsonObject(); 
-			//Read the values from the JSON object
-			String fundId = fundUpdateObject.get("fundId").getAsString(); 
-			String fundCode = fundUpdateObject.get("fundCode").getAsString(); 
-			String fundType = fundUpdateObject.get("fundType").getAsString(); 
-			String amount = fundUpdateObject.get("amount").getAsString(); 
-			String date = fundUpdateObject.get("date").getAsString();
-			String status = fundUpdateObject.get("status").getAsString();
-		 
-			String output = fundObj.updateFund(fundId, fundCode, fundType, amount, date,status); 
-			return output; 
-		}
 		
+		//update fund state by project Manager
+		@PUT
+		@Path("/updateStatus")
+		@Consumes(MediaType.APPLICATION_JSON)
+		@Produces(MediaType.TEXT_PLAIN)
+		public String updateFundStatus(String updateStatus)
+		{
+		
+			//Convert the input string to a JSON object	 
+			JsonObject stObj = new JsonParser().parse(updateStatus).getAsJsonObject();
+		
+			//Read the values from the JSON object	 
+			int fundId = stObj.get("fundId").getAsInt();
+			String status = stObj.get("status").getAsString();
+			
+			String output = fundObj.updateFundStatus(fundId, status);
+		
+			return output;
+		}
 		
 
 }
